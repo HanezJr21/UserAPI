20db7f7882d1452db19faae626df44d322d9fc35
admin <yohanesrinaldy127@gmail.com>
2021-08-10 09:27:53 +0700

    initial commit

---
+++ UserMicroservices/UserMicroservices/Controllers/UserController.cs
@@ -0,0 +1,30 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using UserMicroservices.DBContexts;
+using UserMicroservices.Model;
+
+// For more information on enabling Web API for empty projects, visit https://go.microsoft.com/fwlink/?LinkID=397860
+
+namespace UserMicroservices.Controllers
+{
+    [Route("api/[controller]")]
+    [ApiController]
+    public class UserController : ControllerBase
+    {
+        private MyDBContext myDbContext;
+
+        public UserController(MyDBContext context)
+        {
+            myDbContext = context;
+        }
+
+        [HttpGet]
+        public IList<User> Get()
+        {
+            return (this.myDbContext.Users.ToList());
+        }
+    }
+}
